2713f747f21cc48ee12c409e77a9ba87db9665da
Hugo Zammit <hugo.zammit@invenietis.com>
2017-10-23 17:39:31 +0200

    [WIP] Remove AOT and serve from angular application.

--- src/CK.Glouton.Web/app/src/app/pages/home-page.component.ts
+++ src/CK.Glouton.Web/app/src/app/pages/home-page.component.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
   selector: 'home',
   template: `
     <h3>Home Page</h3>
+    This our home page!
+    Maybe one day, there will be stuff in there...
   `
 })
 export class HomePageComponent {
