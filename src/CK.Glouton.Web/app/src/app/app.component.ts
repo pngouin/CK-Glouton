58dd97669dad8f083397f8211f243e027ada190a
Hugo Zammit <hugo.zammit@invenietis.com>
2017-10-30 16:53:23 +0100

    Add PrimeNG.

--- src/CK.Glouton.Web/app/src/app/app.component.ts
+++ src/CK.Glouton.Web/app/src/app/app.component.ts
@@ -3,6 +3,11 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
+  styleUrls: [
+    '../../node_modules/font-awesome/css/font-awesome.min.css',
+    '../../node_modules/primeng/resources/themes/omega/theme.css',
+    '../../node_modules/primeng/resources/primeng.min.css'
+  ]
 })
 export class AppComponent {
 }
