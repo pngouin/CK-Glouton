54a1aa921fafa23ac2750027ce83e11184eaf3ba
Jilanow <fabulet@intechinfo.fr>
2017-11-03 10:14:24 +0100

    Remove useless method.

--- src/CK.Glouton.Lucene/LuceneConstant.cs
+++ src/CK.Glouton.Lucene/LuceneConstant.cs
@@ -33,21 +33,5 @@ namespace CK.Glouton.Lucene
                 Directory.CreateDirectory(path);
             return path;
         }
-
-        /// <summary>
-        /// Test in the directory of the indexer.
-        /// On windows in appdata, in other os in home.
-        /// If he doesn't find anything create the directory.
-        /// </summary>
-        /// <param name="dirNames"> the string array that contain the AppName, the name of the machine executing the app and the app GUID </param>
-        /// <returns></returns>
-        public static string GetPath(string[] dirNames)
-        {
-            if (dirNames.Length > 3) throw new ArgumentException("The array is too big.", nameof(dirNames));
-            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Glouton", "Logs", dirNames[0], dirNames[1], dirNames[2]);
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-            return path;
-        }
     }
 }
