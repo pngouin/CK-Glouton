51b332d77958b470b160bda7454b153aa50d2e0b
Jilanow <fabulet@intechinfo.fr>
2017-11-06 16:06:56 +0100

    [WIP] start test for lucene

---
+++ Tests/CK.Glouton.Tests.NetFramework/LuceneTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+using CK.Core;
+using CK.Glouton.Handler.Tcp;
+using CK.Monitoring;
+using CK.Monitoring.Handlers;
+using FluentAssertions;
+using NUnit.Framework;
+using CK.Glouton.Lucene;
+
+namespace CK.Glouton.Tests.NetFramework
+{
+    [TestFixture]
+    public class LuceneTests : TestsSetupTeardown
+    {
+        [Test]
+        public void log_can_be_indexed ()
+        {
+            var server = TestHelper.DefaultServer();
+            var m = new ActivityMonitor();
+            m.MinimalFilter = LogFilter.Debug;
+            server.Open();
+            m.Info("Hello world");
+            m.Error("CriticalError");
+            server.Dispose();
+        }
+    }
+}
---
+++ Tests/CK.Glouton.Tests.NetFramework/TestsSetupTeardown.cs
@@ -0,0 +1,60 @@
+﻿using CK.Core;
+using CK.Glouton.Handler.Tcp;
+using CK.Monitoring;
+using CK.Monitoring.Handlers;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Reflection;
+using System.Text;
+
+namespace CK.Glouton.Tests.NetFramework
+{
+    public class TestsSetupTeardown
+    {
+        [SetUp]
+        public void Setup()
+        {
+            if (!System.Console.IsOutputRedirected)
+                System.Console.OutputEncoding = Encoding.UTF8;
+
+            LogFile.RootLogPath = TestHelper.GetTestLogDirectory();
+
+            ActivityMonitor.DefaultFilter = LogFilter.Debug;
+            ActivityMonitor.AutoConfiguration += monitor => monitor.Output.RegisterClient(new ActivityMonitorConsoleClient());
+             
+            var grandOutputConfigurationServer = new GrandOutputConfiguration();
+            grandOutputConfigurationServer.AddHandler(new TextFileConfiguration
+            {
+                MaxCountPerFile = 10000,
+                Path = "Text",
+            });
+            GrandOutput.EnsureActiveDefault(grandOutputConfigurationServer);
+
+            var grandOutputConfigurationClient = new GrandOutputConfiguration();
+            grandOutputConfigurationClient.AddHandler(new TextFileConfiguration
+            {
+                MaxCountPerFile = 10000,
+                Path = "Text",
+            });
+            grandOutputConfigurationClient.AddHandler(new TcpHandlerConfiguration
+            {
+                Host = TestHelper.DefaultHost,
+                Port = TestHelper.DefaultPort,
+                IsSecure = false,
+                AppName = typeof(TestHelper).GetTypeInfo().Assembly.GetName().Name,
+                PresentEnvironmentVariables = true,
+                PresentMonitoringAssemblyInformation = true,
+                HandleSystemActivityMonitorErrors = true
+            });
+            GrandOutput.EnsureActiveDefault(grandOutputConfigurationClient);
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            GrandOutput.Default.Dispose();
+        }
+    }
+}
