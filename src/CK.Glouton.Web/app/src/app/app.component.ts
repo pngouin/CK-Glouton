2e77eb277c607d34e633a71221abee48694e23a9
Hugo Zammit <hugo.zammit@invenietis.com>
2017-11-06 09:54:58 +0100

    Add ViewEncapsulation.None in app component.

--- src/CK.Glouton.Web/app/src/app/app.component.ts
+++ src/CK.Glouton.Web/app/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: [
     '../../node_modules/font-awesome/css/font-awesome.min.css',
     '../../node_modules/primeng/resources/themes/omega/theme.css',
