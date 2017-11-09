0b8c0f5770ccffadc3a6bf8f90d76225bf75734f
ZooPin <battle.raibleur@gmail.com>
2017-11-09 12:17:35 +0100

    [WIP] Add Test server for test

--- Tests/CK.Glouton.Tests.NetFramework/HandlerTest.cs
+++ Tests/CK.Glouton.Tests.NetFramework/HandlerTest.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using CK.Core;
+using FluentAssertions;
 
 namespace CK.Glouton.Tests.NetFramework
 {
@@ -20,8 +22,25 @@ namespace CK.Glouton.Tests.NetFramework
         [TearDown]
         public void TearDown()
         {
-            GrandOuputServerHelper.TearDown();
-            GrandOutputHandlerHelper.TearDown();
+            //GrandOuputServerHelper.TearDown();
+            //GrandOutputHandlerHelper.TearDown();
+        }
+
+        [Test]
+        public void handler_can_send_some_log()
+        {
+            var server = TestHelper.DefaultServer();
+            var m = new ActivityMonitor();
+            m.MinimalFilter = LogFilter.Debug;
+            server.Open();
+
+            var guid = Guid.NewGuid();
+
+            m.Info(guid.ToString);
+            var response = server.GetLogEntry(guid.ToString());
+
+            response.Should().Be(guid.ToString());
+            server.Dispose();
         }
     }
 }
---
+++ Tests/CK.Glouton.Tests.NetFramework/ServerTestHelper.cs
@@ -0,0 +1,91 @@
+﻿using CK.Glouton.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CK.ControlChannel.Abstractions;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using CK.ControlChannel.Tcp;
+using System.IO;
+using CK.Core;
+using CK.Monitoring;
+
+namespace CK.Glouton.Tests
+{
+    public class ServerTestHelper : IGloutonServer, IDisposable
+    {
+        private readonly ControlChannelServer _controlChannelServer;
+        private readonly MemoryStream _memoryStream;
+        private readonly CKBinaryReader _binaryReader;
+        private readonly List<ILogEntry> _listLog;
+
+
+        public ServerTestHelper(
+            string boundIpAddress,
+            int port,
+            IAuthorizationHandler clientAuthorizationHandler = null,
+            X509Certificate2 serverCertificate = null,
+            RemoteCertificateValidationCallback userCertificateValidationCallback = null)
+        {
+            _controlChannelServer = new ControlChannelServer
+            (
+                boundIpAddress,
+                port,
+                clientAuthorizationHandler ?? new TcpAuthHandler(),
+                serverCertificate,
+                userCertificateValidationCallback
+            );
+            _controlChannelServer.RegisterChannelHandler("GrandOutputEventInfo", HandleGrandOutputEventInfo);
+            _memoryStream = new MemoryStream();
+            _binaryReader = new CKBinaryReader(_memoryStream, Encoding.UTF8, true);
+            _listLog = new List<ILogEntry>();
+        }
+
+        private void HandleGrandOutputEventInfo(IActivityMonitor monitor, byte[] data, IServerClientSession clientSession)
+        {
+            var version = Convert.ToInt32(clientSession.ClientData["LogEntryVersion"]);
+
+            _memoryStream.SetLength(0);
+            _memoryStream.Write(data, 0, data.Length);
+            _memoryStream.Seek(0, SeekOrigin.Begin);
+
+            var entry = LogEntry.Read(_binaryReader, version, out _);
+            _listLog.Add(entry);
+        }
+
+        public ILogEntry GetLogEntry(string text)
+        {
+            return _listLog.Single(e => e.Text == text);
+        }
+
+        public void Close()
+        {
+            _controlChannelServer.Open();
+        }
+
+        public void Open()
+        {
+            _controlChannelServer.Close();
+        }
+
+        #region IDisposable Support
+
+        public void Dispose()
+        {
+            _controlChannelServer.Dispose();
+        }
+
+        #endregion
+
+    }
+}
+
+internal class TcpAuthHandler : IAuthorizationHandler
+{
+    public bool OnAuthorizeSession(IServerClientSession s)
+    {
+        return true;
+    }
+}
