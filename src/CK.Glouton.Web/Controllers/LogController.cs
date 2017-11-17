5c6201add85fc804301f565a1ae38f4c71d09c1d
Hugo Zammit <hugo.zammit@invenietis.com>
2017-11-17 10:21:20 +0100

    [WIP] Add GetAll API point. Which means adding services and models.

---
+++ src/CK.Glouton.Web/Controllers/LogController.cs
@@ -0,0 +1,32 @@
+﻿using CK.Glouton.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CK.Glouton.Web.Controllers
+{
+
+    /*
+     * api/log?max=[max] -- GET
+     * api/log/search?query=[query] -- GET
+     * api/log/search/date?from=[date]&to=[date]&monitor=[monitor]&fields=[fields]&keywords=[keywords] -- GET
+     * api/log/monitor?max=[max] -- GET
+     * api/log/app?max=[max] -- GET
+    */
+
+    [Route( "api/log" )]
+    public class LogController : Controller
+    {
+        private readonly ILuceneSearcherService _luceneSearcherService;
+
+        public LogController( ILuceneSearcherService luceneSearcherService )
+        {
+            _luceneSearcherService = luceneSearcherService;
+        }
+
+        [HttpGet]
+        public List<ILogViewModel> GetAll( int max )
+        {
+            return _luceneSearcherService.GetAll( max );
+        }
+    }
+}
--- src/CK.Glouton.Web/Startup.cs
+++ src/CK.Glouton.Web/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CK.Glouton.Model;
+using CK.Glouton.Service;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +18,8 @@ namespace CK.Glouton.Web
 
         public void ConfigureServices( IServiceCollection services )
         {
+            services.AddSingleton<ILuceneSearcherService, LuceneSearcherService>();
+
             services.AddMvc();
         }
 
