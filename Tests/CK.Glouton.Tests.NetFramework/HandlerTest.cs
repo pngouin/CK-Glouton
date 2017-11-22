7de0d63cb894b6580eecc83dd5f2a7213f6568ae
Hugo Zammit <hugo.zammit@invenietis.com>
2017-11-22 14:53:27 +0100

    Add LogEntryExtensions.

--- Tests/CK.Glouton.Tests.NetFramework/HandlerTest.cs
+++ Tests/CK.Glouton.Tests.NetFramework/HandlerTest.cs
@@ -21,7 +21,6 @@ namespace CK.Glouton.Tests
             using( var server = TestHelper.DefaultMockServer() )
             {
                 server.Open();
-                server.ListLog.Clear();
 
                 using( var grandOutputServer = GrandOutputHelper.GetNewGrandOutputServer() )
                 using( var grandOutputClient = GrandOutputHelper.GetNewGrandOutputClient() )
@@ -37,8 +36,7 @@ namespace CK.Glouton.Tests
 
                     Thread.Sleep( 125 );
 
-                    var response = server.GetLogEntry( guid.ToString() );
-                    response.Text.Should().Be( guid.ToString() );
+                    server.GetLogEntry( guid.ToString() ).ValidateLogEntry( guid.ToString() ).Should().BeTrue();
 
                     serverActivityMonitor.CloseGroup();
                     clientActivityMonitor.CloseGroup();
@@ -52,7 +50,6 @@ namespace CK.Glouton.Tests
             using( var server = TestHelper.DefaultMockServer() )
             {
                 server.Open();
-                server.ListLog.Clear();
 
                 using( var grandOutputServer = GrandOutputHelper.GetNewGrandOutputServer() )
                 using( var grandOutputClient = GrandOutputHelper.GetNewGrandOutputClient() )
@@ -81,37 +78,13 @@ namespace CK.Glouton.Tests
 
                     Thread.Sleep( 125 );
 
-                    var initialEntry = server.GetLogEntry( initialMessage );
-                    var debugEntry = server.GetLogEntry( debugMessage );
-                    var traceEntry = server.GetLogEntry( traceMessage );
-                    var warnEntry = server.GetLogEntry( warnMessage );
-                    var errorEntry = server.GetLogEntry( errorMessage );
-                    var fatalEntry = server.GetLogEntry( fatalMessage );
-                    var finalEntry = server.GetLogEntry( finalMessage );
-
-                    initialEntry.Text.Should().Be( initialMessage );
-                    ( initialEntry.LogLevel & LogLevel.Info ).Should().Be( LogLevel.Info );
-
-                    debugEntry.Text.Should().Be( debugMessage );
-                    ( debugEntry.LogLevel & LogLevel.Debug ).Should().Be( LogLevel.Debug );
-
-                    traceEntry.Text.Should().Be( traceMessage );
-                    ( traceEntry.LogLevel & LogLevel.Trace ).Should().Be( LogLevel.Trace );
-
-                    warnEntry.Text.Should().Be( warnMessage );
-                    ( warnEntry.LogLevel & LogLevel.Warn ).Should().Be( LogLevel.Warn );
-
-                    errorEntry.Text.Should().Be( errorMessage );
-                    ( errorEntry.LogLevel & LogLevel.Error ).Should().Be( LogLevel.Error );
-
-                    fatalEntry.Text.Should().Be( fatalMessage );
-                    ( fatalEntry.LogLevel & LogLevel.Fatal ).Should().Be( LogLevel.Fatal );
-
-                    finalEntry.Text.Should().Be( finalMessage );
-                    ( finalEntry.LogLevel & LogLevel.Info ).Should().Be( LogLevel.Info );
-
-                    serverActivityMonitor.CloseGroup();
-                    clientActivityMonitor.CloseGroup();
+                    server.GetLogEntry( initialMessage ).ValidateLogEntry( initialMessage, LogLevel.Info ).Should().BeTrue();
+                    server.GetLogEntry( debugMessage ).ValidateLogEntry( debugMessage, LogLevel.Debug ).Should().BeTrue();
+                    server.GetLogEntry( traceMessage ).ValidateLogEntry( traceMessage, LogLevel.Trace ).Should().BeTrue();
+                    server.GetLogEntry( warnMessage ).ValidateLogEntry( warnMessage, LogLevel.Warn ).Should().BeTrue();
+                    server.GetLogEntry( errorMessage ).ValidateLogEntry( errorMessage, LogLevel.Error ).Should().BeTrue();
+                    server.GetLogEntry( fatalMessage ).ValidateLogEntry( fatalMessage, LogLevel.Fatal ).Should().BeTrue();
+                    server.GetLogEntry( finalMessage ).ValidateLogEntry( finalMessage, LogLevel.Info ).Should().BeTrue();
                 }
             }
         }
@@ -154,14 +127,9 @@ namespace CK.Glouton.Tests
 
                             Thread.Sleep( 125 );
 
-                            var response1 = server.GetLogEntry( guid1.ToString() );
-                            response1.Text.Should().Be( guid1.ToString() );
-
-                            var response2 = server.GetLogEntry( guid2.ToString() );
-                            response2.Text.Should().Be( guid2.ToString() );
-
-                            var response3 = server.GetLogEntry( guid3.ToString() );
-                            response3.Text.Should().Be( guid3.ToString() );
+                            server.GetLogEntry( guid1.ToString() ).ValidateLogEntry( guid1.ToString() ).Should().BeTrue();
+                            server.GetLogEntry( guid2.ToString() ).ValidateLogEntry( guid2.ToString() ).Should().BeTrue();
+                            server.GetLogEntry( guid3.ToString() ).ValidateLogEntry( guid3.ToString() ).Should().BeTrue();
 
                             serverActivityMonitor.CloseGroup();
                             clientActivityMonitor1.CloseGroup();
