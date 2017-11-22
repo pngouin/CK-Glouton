a2480a8eeb692519e67c5c47fc836e34ddb27990
Jilanow <fabulet@intechinfo.fr>
2017-11-22 17:30:23 +0100

    Proper lucene tests, creating logs, searching it and delete it

--- Tests/CK.Glouton.Tests.NetFramework/LuceneTests.cs
+++ Tests/CK.Glouton.Tests.NetFramework/LuceneTests.cs
@@ -1,6 +1,11 @@
 ﻿using CK.Core;
 using NUnit.Framework;
+using FluentAssertions;
 using System.Threading;
+using CK.Glouton.Lucene;
+using System.Reflection;
+using System;
+using System.IO;
 
 namespace CK.Glouton.Tests
 {
@@ -11,10 +16,12 @@ namespace CK.Glouton.Tests
         public void SetUp()
         {
             TestHelper.Setup();
+            var logDir = Assembly.GetExecutingAssembly().FullName.Split(',')[0];
+            Array.ForEach(Directory.GetFiles(LuceneConstant.GetPath(logDir)), File.Delete);
         }
 
         [Test]
-        public void log_can_be_indexed()
+        public void log_can_be_indexed_and_searched()
         {
             using( var server = TestHelper.DefaultGloutonServer() )
             {
@@ -29,6 +36,14 @@ namespace CK.Glouton.Tests
                 Thread.Sleep(500);
                 g.Dispose();
             }
+            var logDir = Assembly.GetExecutingAssembly().FullName.Split(',')[0];
+
+            LuceneSearcher searcher = new LuceneSearcher(LuceneConstant.GetPath(logDir), new[] { "LogLevel", "Text" });
+            var topdoc = searcher.Search("Hello world");
+            Assert.That(topdoc != null);
+            topdoc = null;
+            topdoc = searcher.Search("CriticalError");
+            Assert.That(topdoc != null);
         }
     }
 }
