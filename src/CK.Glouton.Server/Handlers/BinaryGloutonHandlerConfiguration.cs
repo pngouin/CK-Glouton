c53a526f51a80c2347619df7a5ae580ed16f0caf
ZooPin <battle.raibleur@gmail.com>
2017-12-01 11:45:46 +0100

    Add local FileBaseConfiguration to avoid conflict with future modifications in FileBaseConfiguration in CK.Monitoring.

--- src/CK.Glouton.Server/Handlers/BinaryGloutonHandlerConfiguration.cs
+++ src/CK.Glouton.Server/Handlers/BinaryGloutonHandlerConfiguration.cs
@@ -1,24 +1,13 @@
-﻿using CK.Glouton.Model.Server;
-using CK.Monitoring;
-using CK.Monitoring.Handlers;
+﻿
+using CK.Glouton.Model.Server;
 
 namespace CK.Glouton.Server.Handlers
 {
-    public class BinaryGloutonHandlerConfiguration : FileConfigurationBase, IGloutonHandlerConfiguration
+    public class BinaryGloutonHandlerConfiguration : FileConfigurationBase
     {
         public bool UseGzipCompression { get; set; }
 
-        public override IHandlerConfiguration Clone()
-        {
-            return CloneThis();
-        }
-
-        IGloutonHandlerConfiguration IGloutonHandlerConfiguration.Clone()
-        {
-            return CloneThis();
-        }
-
-        private BinaryGloutonHandlerConfiguration CloneThis()
+        public override IGloutonHandlerConfiguration Clone()
         {
             return new BinaryGloutonHandlerConfiguration
             {
