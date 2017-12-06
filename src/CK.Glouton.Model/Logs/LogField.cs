e2db845193b331a21b7878a5850205781cd3b1fa
ZooPin <battle.raibleur@gmail.com>
2017-12-06 15:59:29 +0100

    Add LogField that regroup all constant for log field in Lucene.

---
+++ src/CK.Glouton.Model/Logs/LogField.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CK.Glouton.Model.Logs
+{
+    public class LogField
+    {
+        public const string LOG_TYPE = "LogType";
+        public const string EXCEPTION = "Exception";
+        public const string LOG_TIME = "LogTime";
+        public const string LOG_LEVEL = "LogLevel";
+        public const string TAGS = "Tags";
+        public const string SOURCE_FILE_NAME = "FileName";
+        public const string MONITOR_ID = "MonitorId";
+        public const string GROUP_DEPTH = "GroupDepth";
+        public const string PREVIOUS_ENTRY_TYPE = "PreviousEntryType";
+        public const string APP_ID = "AppId";
+        public const string INNER_EXCEPTION = "InnerException";
+        public const string CONCLUSION = "Conclusion";
+        public const string TEXT = "Text";
+        public const string PREVIOUS_LOG_TIME = "PreviousLogTime";
+        public const string LINE_NUMBER = "LineNumber";
+        public const string APP_NAME = "AppName";
+        public const string INDEX_DTS = "IndexDTS";
+        public const string STACK = "Stack";
+        public const string DETAILS = "Details";
+        public const string MONITOR_ID_LIST = "MonitorIdList";
+        public const string APP_NAME_LIST = "AppNameList";
+        public const string DATE_TIME_STAMP = "DateTimeStamp";
+        public const string CK_EXCEPTION_DATA = "CKExceptionData";
+        public const string CK_TRAIT = "CKTrait";
+        public const string AGGREGATED_EXCEPTIONS = "AggregatedExceptions";
+        public const string EXCEPTION_DEPTH = "ExceptionDepth";
+    }
+}
