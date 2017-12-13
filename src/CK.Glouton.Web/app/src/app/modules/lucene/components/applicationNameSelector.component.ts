b293d9178971f0f72796e5ec4246033631ae98e6
Hugo Zammit <hugo.zammit@invenietis.com>
2017-12-13 10:07:08 +0100

    Add actions behavior for applicationNameSelector.

--- src/CK.Glouton.Web/app/src/app/modules/lucene/components/applicationNameSelector.component.ts
+++ src/CK.Glouton.Web/app/src/app/modules/lucene/components/applicationNameSelector.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { EffectDispatcher } from '@ck/rx';
+import { IAppState } from 'app/app.state';
 import { LogService } from 'app/_services';
 import { ILogView } from 'app/common/logs/models';
+import { SubmitAppNamesEffect } from '../actions';
 
 @Component({
     selector: 'applicationNameSelector',
@@ -9,16 +15,23 @@ import { ILogView } from 'app/common/logs/models';
 
 export class ApplicationNameSelectorComponent implements OnInit {
 
+    private _applicationNames$: Observable<string[]>;
     private _applicationNames: string[];
+
     private _selected: string[];
+    private _subscription: Subscription;
 
     constructor(
-        private logService: LogService
+        private logService: LogService,
+        private effectDispatcher: EffectDispatcher,
+        private store: Store<IAppState>
     ) {
+        this._applicationNames$ = this.store.select(s => s.luceneParameters.appNames);
+        this._subscription = this._applicationNames$.subscribe(a => this._selected = a);
     }
 
     onChange(_: boolean): void {
-        console.log(this._selected);
+        this.effectDispatcher.dispatch(new SubmitAppNamesEffect(this._selected));
     }
 
     ngOnInit(): void {
