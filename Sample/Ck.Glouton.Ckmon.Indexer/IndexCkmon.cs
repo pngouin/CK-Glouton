76d6faa996db92a5f9f450eba4b91a69dab76393
ZooPin <battle.raibleur@gmail.com>
2017-12-15 11:31:01 +0100

    Add new sample project to index .ckmon file.

---
+++ Sample/Ck.Glouton.Ckmon.Indexer/IndexCkmon.cs
@@ -0,0 +1,35 @@
+﻿using CK.Core;
+using CK.Glouton.Lucene;
+using CK.Monitoring;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Ck.Glouton.Ckmon.Indexer
+{
+    public class IndexCkmon
+    {
+        public IndexCkmon(LuceneConfiguration configuration)
+        {
+            string path = GetFiles().First();
+            using (var indexer = new LuceneIndexer(configuration))
+            using (LogReader reader = LogReader.Open(path))
+            {
+                reader.MoveNext();
+                for (; ; )
+                {
+                    indexer.IndexLog(reader.CurrentMulticast, configuration.Directory);
+                    if (!reader.MoveNext())
+                        return;
+                }
+            }
+        }
+
+        private string[] GetFiles ()
+        {
+            return Directory.GetFiles("./ckmon", "*.ckmon", SearchOption.TopDirectoryOnly);
+        }
+    }
+}
---
+++ Sample/Ck.Glouton.Ckmon.Indexer/Program.cs
@@ -0,0 +1,20 @@
+﻿using CK.Glouton.Lucene;
+using System;
+using System.Reflection;
+
+namespace Ck.Glouton.Ckmon.Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".ckmon need to be in ./ckmon");
+            LuceneConfiguration configuration = new LuceneConfiguration
+            {
+                MaxSearch = 100,
+                Directory = Assembly.GetExecutingAssembly().GetName().Name
+            };
+            IndexCkmon readCkmon = new IndexCkmon(configuration);
+        }
+    }
+}
