1b91504cd184ef2f0a6838c33542bddb9bf84e9d
ZooPin <battle.raibleur@gmail.com>
2017-12-18 14:06:24 +0100

    [Fix] IndexCkmon handle correctly multiple .ckmon file.

--- Sample/Ck.Glouton.Ckmon.Indexer/Program.cs
+++ Sample/Ck.Glouton.Ckmon.Indexer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CK.Glouton.Lucene;
 using System;
+using System.IO;
 using System.Reflection;
 
 namespace Ck.Glouton.Ckmon.Indexer
@@ -9,12 +10,21 @@ namespace Ck.Glouton.Ckmon.Indexer
         static void Main(string[] args)
         {
             Console.WriteLine(".ckmon need to be in ./ckmon");
-            LuceneConfiguration configuration = new LuceneConfiguration
+            foreach (var path in GetFiles())
             {
-                MaxSearch = 100,
-                Directory = Assembly.GetExecutingAssembly().GetName().Name
-            };
-            IndexCkmon readCkmon = new IndexCkmon(configuration);
+                string appName = "ckmon-" + Guid.NewGuid().ToString().Substring(0, 8);
+                LuceneConfiguration configuration = new LuceneConfiguration
+                {
+                    MaxSearch = 100,
+                    Directory = appName
+                };
+                IndexCkmon readCkmon = new IndexCkmon(configuration, path);
+            }
+        }
+
+        private static string[] GetFiles()
+        {
+            return Directory.GetFiles("./ckmon", "*.ckmon", SearchOption.TopDirectoryOnly);
         }
     }
 }
