80cb195ce8a6f9fa4fc00cb556dcbb47b7eba53d
ZooPin <battle.raibleur@gmail.com>
2017-12-18 22:18:12 +0100

    Add logException component.

--- src/CK.Glouton.Web/app/src/app/modules/lucene/components/index.ts
+++ src/CK.Glouton.Web/app/src/app/modules/lucene/components/index.ts
@@ -4,3 +4,4 @@ export * from './querySearchBar.component';
 export * from './logViewer.component';
 export * from './log.component';
 export * from './logComplex.component';
+export * from './logException.component';
