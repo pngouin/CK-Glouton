94b4d4a6fcef1af00e2ea269f99ab891d5d05d72
ZooPin <battle.raibleur@gmail.com>
2018-01-05 16:18:10 +0100

    Add LuceneStatistics.

---
+++ src/CK.Glouton.Lucene/LuceneStatistics.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK.Glouton.Lucene
+{
+    public class LuceneStatistics
+    {
+        LuceneSearcherManager _luceneSearcher;
+        LuceneSearcherConfiguration _allSearchLog;
+        LuceneSearcherConfiguration _allSearchException;
+
+        public LuceneStatistics ( LuceneConfiguration configuration )
+        {
+            _luceneSearcher = new LuceneSearcherManager(configuration);
+            _allSearchLog = new LuceneSearcherConfiguration
+            {
+                MaxResult = int.MaxValue
+            };
+            _allSearchLog.SearchAll(Model.Logs.LuceneWantAll.Log);
+
+            _allSearchException = new LuceneSearcherConfiguration
+            {
+                MaxResult = int.MaxValue
+            };
+            _allSearchException.SearchAll(Model.Logs.LuceneWantAll.Exception);
+        }
+
+
+        public int LogInAppNameCount (string appName)
+        {
+            if (!_luceneSearcher.AppName.Contains(appName)) return 0;
+            return _luceneSearcher.GetSearcher(appName).Search(_allSearchLog).Count;
+        }
+
+        public int AllLogCount () => _luceneSearcher.GetSearcher(_luceneSearcher.AppName.ToArray()).Search(_allSearchLog).Count;
+
+        public int AllExceptionCount () => _luceneSearcher.GetSearcher(_luceneSearcher.AppName.ToArray()).Search(_allSearchException).Count;
+
+        public IEnumerable<string> GetAppName => _luceneSearcher.AppName;
+        public int AppNameCount() => _luceneSearcher.AppName.Count;
+
+    }
+}
