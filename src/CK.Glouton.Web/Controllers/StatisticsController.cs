44806f14481d1917b7126acc49e07ba22eb57f0d
ZooPin <battle.raibleur@gmail.com>
2018-01-05 17:08:00 +0100

    Add api point to controller.

--- src/CK.Glouton.Web/Controllers/StatisticsController.cs
+++ src/CK.Glouton.Web/Controllers/StatisticsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CK.Glouton.Lucene;
+using CK.Glouton.Model.Lucene;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CK.Glouton.Web.Controllers
@@ -10,11 +11,34 @@ namespace CK.Glouton.Web.Controllers
     [Route("api/stats")]
     public class StatisticsController : Controller
     {
-        LuceneStatistics luceneStatistics;
-        public StatisticsController()
+        private readonly ILuceneStatisticsService _luceneStatistics;
+        public StatisticsController(ILuceneStatisticsService luceneStatistics)
         {
-
+            _luceneStatistics = luceneStatistics;
         }
 
+        [HttpGet("logperappname")]
+        public Dictionary<string, int> LogPerAppName()
+        {
+            return _luceneStatistics.GetLogByAppName();
+        }
+
+        [HttpGet("exceptionperappname")]
+        public Dictionary<string, int> ExceptionPerAppName()
+        {
+            return _luceneStatistics.GetExceptionByAppName();
+        }
+
+        [HttpGet("Log")]
+        public int AllLogCount() => _luceneStatistics.AllLogCount();
+
+        [HttpGet("AppName")]
+        public int AppNameCount() => _luceneStatistics.AppNameCount;
+
+        [HttpGet("Exception")]
+        public int AllException() => _luceneStatistics.AllExceptionCount;
+
+        [HttpGet("AppNames")]
+        public IEnumerable<string> AppNames() => _luceneStatistics.GetAppNames;
     }
 }
