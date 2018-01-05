98906fa961c97baa069136fe7498b05745b85ce0
ZooPin <battle.raibleur@gmail.com>
2018-01-05 16:46:00 +0100

    Add StatisticsController.

---
+++ src/CK.Glouton.Web/Controllers/StatisticsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CK.Glouton.Lucene;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CK.Glouton.Web.Controllers
+{
+    [Route("api/stats")]
+    public class StatisticsController : Controller
+    {
+        LuceneStatistics luceneStatistics;
+        public StatisticsController()
+        {
+
+        }
+
+    }
+}
