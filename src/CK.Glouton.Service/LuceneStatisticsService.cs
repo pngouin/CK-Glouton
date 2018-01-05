066f89ef19c76b7424663671c2c73ed47f52afa0
ZooPin <battle.raibleur@gmail.com>
2018-01-05 16:39:05 +0100

    Add LuceneStatisticsService.

---
+++ src/CK.Glouton.Service/LuceneStatisticsService.cs
@@ -0,0 +1,46 @@
+﻿using CK.Glouton.Lucene;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CK.Glouton.Service
+{
+    public class LuceneStatisticsService
+    {
+        private readonly LuceneConfiguration _configuration;
+        private readonly LuceneStatistics _stats;
+
+        public LuceneStatisticsService(IOptions<LuceneConfiguration> configuration)
+        {
+            _configuration = configuration.Value;
+            _stats = new LuceneStatistics(_configuration);
+        }
+
+        public int AllLogCount() => _stats.AllLogCount;
+        public int AllExceptionCount => _stats.AllExceptionCount;
+        public int AppNameCount => _stats.AppNameCount;
+        public IEnumerable<string> GetAppNames => _stats.GetAppNames;
+
+        public Dictionary<string, int> GetLogByAppName ()
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            foreach (var appName in _stats.GetAppNames)
+            {
+                result.Add(appName, _stats.LogInAppNameCount(appName));
+            }
+            return result;
+        }
+
+        public Dictionary<string, int> GetExceptionByAppName ()
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            foreach (var appName in _stats.GetAppNames)
+            {
+                result.Add(appName, _stats.ExceptionInAppNameCount(appName));
+            }
+            return result;
+        }
+        
+    }
+}
