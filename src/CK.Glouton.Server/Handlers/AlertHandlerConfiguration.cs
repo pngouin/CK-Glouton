25eda8d06a418c63e9e0b517f730792c625b2907
Hugo Zammit <hugo.zammit@invenietis.com>
2018-01-08 17:39:48 +0100

    Add basic of AlertHandler.

---
+++ src/CK.Glouton.Server/Handlers/AlertHandlerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using CK.Glouton.Model.Server;
+using System;
+using System.Collections.Generic;
+
+namespace CK.Glouton.Server.Handlers
+{
+    public class AlertHandlerConfiguration : IGloutonHandlerConfiguration
+    {
+        public List<(Func<ReceivedData, bool> condition, IAlertSender sender)> Alerts { get; set; }
+
+        public IGloutonHandlerConfiguration Clone()
+        {
+            return new AlertHandlerConfiguration
+            {
+                Alerts = Alerts
+            };
+        }
+    }
+}
