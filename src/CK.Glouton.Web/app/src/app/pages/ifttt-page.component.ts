cb968c468c9259b494cc488deaa6eaacf9458eb9
ZooPin <battle.raibleur@gmail.com>
2018-01-15 16:52:57 +0100

    Add ifttt page.

---
+++ src/CK.Glouton.Web/app/src/app/pages/ifttt-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, ChangeDetectorRef } from '@angular/core';
+
+@Component({
+  selector: 'ifttt-page',
+  template: `
+    <div>
+      <ifttt></ifttt>
+    </div>
+  `
+})
+export class IftttPageComponent {
+}
