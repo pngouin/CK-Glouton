9efa2367db7f1c3dbbba9ec1c888b8f560977c6c
Hugo Zammit <hugo.zammit@invenietis.com>
2018-01-22 14:47:46 +0100

    [WIP] Some tweaks on AlertService.

--- src/CK.Glouton.Service/AlertService.cs
+++ src/CK.Glouton.Service/AlertService.cs
@@ -1,39 +1,35 @@
-﻿using System;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using CK.ControlChannel.Tcp;
+﻿using CK.ControlChannel.Tcp;
 using CK.Glouton.Model.Server.Handlers;
 using CK.Glouton.Model.Server.Sender;
 using CK.Glouton.Model.Services;
 using Microsoft.Extensions.Options;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace CK.Glouton.Service
 {
     public class AlertService : IAlertService
     {
-        private readonly ControlChannelClient _controlChannelCLient;
+        private readonly ControlChannelClient _controlChannelClient;
         private readonly TcpControlChannelConfiguration _configuration;
         private readonly IFormatter _formatter;
         private readonly MemoryStream _memoryStream;
 
 
-        public string[] AvailableConfiguration
-        {
-            get => new string[] { "Mail" } ;
-        }
+        public string[] AvailableConfiguration => new[] { "Mail" };
 
-        public AlertService(IOptions<TcpControlChannelConfiguration> configuration)
+        public AlertService( IOptions<TcpControlChannelConfiguration> configuration )
         {
             _configuration = configuration.Value;
-            _controlChannelCLient = new ControlChannelClient(
+            _controlChannelClient = new ControlChannelClient(
                 _configuration.Host,
                 _configuration.Port,
                 _configuration.BuildAuthData(),
                 _configuration.IsSecure
                 );
 
-            _controlChannelCLient.OpenAsync();
+            _controlChannelClient.OpenAsync().GetAwaiter().GetResult();
 
             _memoryStream = new MemoryStream();
             _formatter = new BinaryFormatter();
@@ -41,11 +37,11 @@ namespace CK.Glouton.Service
 
         public bool SendNewAlert( IAlertExpressionModel alertExpression )
         {
-            _memoryStream.Seek(0, SeekOrigin.Begin);
+            _memoryStream.Seek( 0, SeekOrigin.Begin );
             _memoryStream.Flush();
 
-            _formatter.Serialize(_memoryStream, alertExpression);
-            _controlChannelCLient.SendAsync("AddAlertSender", _memoryStream.ToArray()).GetAwaiter().GetResult();
+            _formatter.Serialize( _memoryStream, alertExpression );
+            _controlChannelClient.SendAsync( "AddAlertSender", _memoryStream.ToArray() ).GetAwaiter().GetResult();
 
             return true;
         }
