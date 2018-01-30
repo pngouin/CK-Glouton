1f224a400fd99ab033479c63f8873671368641f5
ZooPin <battle.raibleur@gmail.com>
2018-01-30 13:48:24 +0100

    Fix alertViewer component.

--- src/CK.Glouton.Web/app/src/app/modules/ifttt/components/alertViewer.component.ts
+++ src/CK.Glouton.Web/app/src/app/modules/ifttt/components/alertViewer.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { IAlertData } from 'app/modules/ifttt/models/expression.model';
+import { IAlertData, IAlertDataViewModel, ISenderModel } from 'app/modules/ifttt/models/expression.model';
 import { IftttService } from 'app/_services';
 import { error } from 'util';
 import { MessageService } from 'primeng/components/common/messageservice';
+import { IAlertExpressionModel } from 'app/modules/ifttt/models/sender.model';
 
 @Component({
     selector: 'alertviewer',
@@ -13,23 +14,39 @@ import { MessageService } from 'primeng/components/common/messageservice';
 export class AlerViewerComponent implements OnInit {
     constructor(private iftttService: IftttService, private messageService: MessageService) { }
 
-    data: IAlertData[];
+    alerts: IAlertData[];
+    data: IAlertDataViewModel[] = [];
 
     ngOnInit(): void {
         this.update();
     }
 
     update(): void {
-        this.iftttService.getAlert().subscribe(d => this.data = d,
+        this.iftttService.getAlert().subscribe(d => this.createDataViewModel(d),
             error => this.messageService.add({
                 severity: 'error', summary: 'Error', detail: `An error have occured(${error.message})`
             }));
     }
 
-    concatenateSender(senders: string[]): string {
+
+    private createDataViewModel(alerts: IAlertData[]): void {
+        let alertStr: string;
+        for (let alert of alerts) {
+            alertStr = this.concatenateSender(alert.senders);
+            this.data.push({
+                expressions: alert.expressions,
+                senders: alertStr
+            });
+        }
+    }
+
+    private concatenateSender(senders: ISenderModel[]): string {
         let s: string = '';
         for (let sender of senders) {
-            s.concat(`${sender},`);
+            if (sender.senderType === undefined) {
+                continue;
+            }
+            s += `${sender.senderType} `;
         }
         return s;
     }
