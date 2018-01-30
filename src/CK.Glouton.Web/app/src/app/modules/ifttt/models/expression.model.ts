05325bac1525b260c8166a28dbc0686f982075a6
ZooPin <battle.raibleur@gmail.com>
2018-01-30 11:25:54 +0100

    Add model.

--- src/CK.Glouton.Web/app/src/app/modules/ifttt/models/expression.model.ts
+++ src/CK.Glouton.Web/app/src/app/modules/ifttt/models/expression.model.ts
@@ -1,5 +1,15 @@
 export interface IExpression {
-    Field : string,
-    Operation : string,
-    Body : string
+    Field : string;
+    Operation : string;
+    Body : string;
+}
+
+export interface IAlertData {
+    expression: IExpression[];
+    senders: [
+        {
+            senderType: string;
+            configuration: any;
+        }
+    ];
 }
