f7da61d5428ef39a20c8bb0fe930607af65ee52a
ZooPin <battle.raibleur@gmail.com>
2018-01-30 11:27:40 +0100

    Update ifttt service and update import module.

--- src/CK.Glouton.Web/app/src/app/modules/ifttt/services/ifttt.service.ts
+++ src/CK.Glouton.Web/app/src/app/modules/ifttt/services/ifttt.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IAlertExpressionModel } from 'app/modules/ifttt/models/sender.model';
+import { IAlertData } from 'app/modules/ifttt/models/expression.model';
 
 @Injectable()
 export class IftttService {
@@ -20,5 +21,9 @@ export class IftttService {
 
     sendAlert ( expressionModel : IAlertExpressionModel): Observable<void> {
         return this.httpClient.post<void>(`${this.endpoint}/add`, expressionModel);
+    }
+
+    getAlert (): Observable<IAlertData[]> {
+        return this.httpClient.get<IAlertData[]>(`${this.endpoint}/all`);
     }
 }
